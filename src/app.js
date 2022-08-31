01655e75326f4d191af7462e978be7459b2818de
Đức Anh <84672037+DucAnh1610@users.noreply.github.com>
2022-08-31 12:50:04 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -101,6 +101,6 @@ app.get('/test', (req, res) => {
     res.render('test', {user: user})
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT)
 // console.log('Server is running at ' + PORT)
